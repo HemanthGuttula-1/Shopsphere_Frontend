ce6ffe56c413f30947c9999296802ffd1279cc8b
Hemanth <hemanthguttula1@gmai.com>
2026-07-07 12:41:30 +0530

    Revert "filterAndsearch"
    This reverts commit 91975482d2009b889185bd8b4f49add3a65e2e37.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,97 +13,66 @@ function Navbar() {
   const user = useSelector(state => state.auth.user)
 
   return (
-    <nav className="bg-slate-900 text-white shadow-lg">
+    <nav>
 
-      <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
+      <Link to="/">
+        ShopSphere
+      </Link>
 
-        {/* Logo */}
-        <Link
-          to="/"
-          className="text-3xl font-bold text-cyan-400 hover:text-cyan-300 transition"
-        >
-          ShopSphere
-        </Link>
-
-        {/* Navigation Links */}
-        <div className="flex items-center gap-6">
-
-          {user?.role === "admin" && (
+      {
+        user?.role === "admin" && (
             <>
-              <Link
-                to="/admin"
-                className="hover:text-yellow-400 transition"
-              >
-                Admin
-              </Link>
+                <Link to="/admin">
+                  Admin
+                </Link>
 
-              <Link
-                to="/admin/orders"
-                className="bg-purple-600 hover:bg-purple-700 px-4 py-2 rounded-lg transition"
-              >
-                Manage Orders
-              </Link>
+                <Link
+                  to="/admin/orders"
+                  className="bg-purple-600 text-white px-4 py-2 rounded"
+                >
+                  Manage Orders
+                </Link>
             </>
-          )}
+        )
 
-          <Link
-            to="/cart"
-            className="hover:text-cyan-400 transition"
-          >
-            🛒 Cart ({cartItems.length})
+      }
+
+      <Link to="/cart">
+        Cart ({cartItems.length})
+      </Link>
+
+      <Link to="/wishlist">
+        Wishlist
+      </Link>
+
+      {!token ? (
+        <>
+          <Link to="/login">
+            Login
           </Link>
 
-          <Link
-            to="/wishlist"
-            className="hover:text-pink-400 transition"
-          >
-            ❤️ Wishlist
+          <Link to="/register">
+            Register
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link to="/orders">
+            Orders
+          </Link>
+          <Link to='/checkout'>
+            Pay Now 
           </Link>
 
-          {!token ? (
-            <>
-              <Link
-                to="/login"
-                className="hover:text-green-400 transition"
-              >
-                Login
-              </Link>
-
-              <Link
-                to="/register"
-                className="bg-cyan-500 hover:bg-cyan-600 px-4 py-2 rounded-lg transition"
-              >
-                Register
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link
-                to="/orders"
-                className="hover:text-yellow-400 transition"
-              >
-                Orders
-              </Link>
-
-              <Link
-                to="/checkout"
-                className="bg-green-500 hover:bg-green-600 px-4 py-2 rounded-lg transition"
-              >
-                💳 Pay Now
-              </Link>
-
-              <button
-                onClick={() => dispatch(logout())}
-                className="bg-red-500 hover:bg-red-600 px-4 py-2 rounded-lg transition"
-              >
-                Logout
-              </button>
-            </>
-          )}
-
-        </div>
-
-      </div>
+          <button
+            onClick={() =>
+              dispatch(logout())
+            }
+          >
+            Logout
+          </button>
+        </>
+      )}
 
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,206 +6,56 @@ import ProductCard from "../components/ProductCard";
 function Home() {
   const [search, setSearch] = useState("");
 
-  const [products, setProducts] = useState([]);
-
-  const [category, setCategory] = useState("");
-
-  const [totalPages, setTotalPages] = useState(0);
-
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const [totalProducts, setTotalProducts] = useState(0);
-
-  const [sort, setSort] = useState("newest")
-
-  const [minPrice, setMinPrice] = useState("")
-
-  const [maxPrice, setMaxPrice] = useState("")
+  const [products, setProducts] =
+    useState([]);
 
   const fetchProducts = async () => {
-   
     try {
+      const { data } =
+        await API.get("/products");
 
-      const { data } =  await API.get(`/products`,{
-        params:{
-          search:search,
-          category:category,
-          page:currentPage,
-          limit:8,
-          minPrice:minPrice,
-          maxPrice:maxPrice,
-          sort:sort,
-        }
-      });
-      setProducts(data.products);
-      setTotalPages(data.totalPages);
-      setCurrentPage(data.currentPage);
-      setTotalProducts(data.totalProducts);
-
-      console.log(data)
+      setProducts(data);
     } catch (error) {
       console.log(error);
     }
   };
 
-  useEffect(() => {
-    const token = setTimeout( () => fetchProducts(), 500);
+  const filteredProducts =
+  products.filter((product) =>
+    product.name
+      .toLowerCase()
+      .includes(
+        search.toLowerCase()
+      )
+  );
 
-    return () =>{
-      clearTimeout(token);
-    }
-  }, [search, category, currentPage]);//whenever current page change re run the search 
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
-    <div className="max-w-7xl mx-auto p-6">
+    <div>
 
-      {/* Heading */}
-      <div className="mb-8">
-        <h1 className="text-4xl font-bold text-slate-800">
-          ShopSphere Products
-        </h1>
+      <h1>
+        ShopSphere Products
+      </h1>
+      <input
+        placeholder="Search Product"
+        value={search}
+        onChange={(e) =>
+          setSearch(e.target.value)
+        }
+      />
 
-        <p className="text-gray-500 mt-2">
-          {totalProducts} Products Found
-        </p>
-      </div>
-
-      {/* Search & Filter */}
-      <div className="flex flex-col md:flex-row gap-4 mb-8">
-
-        <input
-          type="text"
-          placeholder="🔍 Search Products..."
-          value={search}
-          onChange={(e) =>{
-              setSearch(e.target.value)
-              setCurrentPage(1)
-            }
-          }
-          className="flex-1 border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-cyan-500"
+      {products.map((product) => (
+        <ProductCard
+          key={product._id}
+          product={product}
         />
-
-        <select
-          value={category}
-          onChange={(e) =>{
-              setCategory(e.target.value)
-              setCurrentPage(1)
-            }
-          }
-          className="border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-cyan-500"
-        >
-
-          <option value="">
-            All Categories
-          </option>
-
-          <option value="Mobiles">
-            Mobiles
-          </option>
-
-          <option value="Laptops">
-            Laptops
-          </option>
-
-          <option value="Shoes">
-            Shoes
-          </option>
-
-          <option value="Watches">
-            Watches
-          </option>
-
-        </select>
-
-      </div>
-
-      <div className="flex flex-col md:flex-row gap-4 mb-8">
-
-  <input
-    type="number"
-    placeholder="Min Price"
-    value={minPrice}
-    onChange={(e) => {
-
-      setMinPrice(e.target.value);
-
-      setCurrentPage(1);
-
-    }}
-    className="border border-gray-300 rounded-lg p-3 w-full"
-  />
-
-  <input
-      type="number"
-      placeholder="Max Price"
-      value={maxPrice}
-      onChange={(e) => {
-
-        setMaxPrice(e.target.value);
-
-        setCurrentPage(1);
-
-      }}
-      className="border border-gray-300 rounded-lg p-3 w-full"
-  />
-
-  </div>
-      {/* Product Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-
-        {products.length > 0 ? (
-
-          products.map((product) => (
-
-            <ProductCard
-              key={product._id}
-              product={product}
-            />
-
-          ))
-
-        ) : (
-
-          <div className="col-span-full text-center text-gray-500 text-xl py-10">
-            No Products Found
-          </div>
-
-        )}
-
-      </div>
-
-      {/* Pagination Info */}
-      <div className="flex justify-between items-center mt-10 border-t pt-5">
-
-        <button
-            disabled={currentPage === 1}
-            onClick={() =>
-                setCurrentPage(currentPage - 1)
-            }
-            className="bg-slate-700 text-white px-4 py-2 rounded disabled:opacity-50"
-        >
-            Previous
-        </button>
-
-        <p className="font-semibold">
-            Page {currentPage} of {totalPages}
-        </p>
-
-        <button
-            disabled={
-                currentPage === totalPages
-            }
-            onClick={() =>
-                setCurrentPage(currentPage + 1)
-            }
-            className="bg-slate-700 text-white px-4 py-2 rounded disabled:opacity-50"
-        >
-            Next
-        </button>
-
-      </div>
+      ))}
 
     </div>
   );
 }
+
 export default Home;
